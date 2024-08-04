e14deb951181a36de71c59efab5655a04f77b8db
jordburg <98363869+jordburg@users.noreply.github.com>
2024-08-04 12:35:06 -0700

    Update LedController.py

--- LedController.py
+++ LedController.py
@@ -12,17 +12,14 @@ NUM_LEDS = 2000  # Total number of LEDs
 ORDER = neopixel.GRB
 
 # Create NeoPixel object
-pixels = neopixel.NeoPixel(
-    LED_PIN, NUM_LEDS, auto_write=False, pixel_order=ORDER)
+pixels = neopixel.NeoPixel(LED_PIN, NUM_LEDS, auto_write=False, pixel_order=ORDER)
 
 # Initialize LED state
 led_state = [(0, 0, 0)] * NUM_LEDS
 
-
 @app.route('/')
 def index():
     return render_template('index.html', num_leds=NUM_LEDS)
-
 
 @app.route('/set_led', methods=['POST'])
 def set_led():
@@ -43,14 +40,12 @@ def set_led():
     pixels.show()
     return jsonify({'success': True})
 
-
 @app.route('/save_configuration', methods=['POST'])
 def save_configuration():
     config_name = request.form['name']
     with open(f'Database/{config_name}.json', 'w') as f:
         json.dump(led_state, f)
     return jsonify({'success': True})
-
 
 @app.route('/load_configuration', methods=['GET'])
 def load_configuration():
@@ -62,6 +57,10 @@ def load_configuration():
     pixels.show()
     return jsonify({'success': True})
 
+@app.route('/list_configurations', methods=['GET'])
+def list_configurations():
+    files = [f for f in os.listdir('Database') if f.endswith('.json')]
+    return jsonify(files)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
